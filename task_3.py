88f1c1d8e541459c5135bc3d12c39dff18e57b2b
Oleksii Hayvoronskyi <oleksiihayvoronskyi@gmail.com>
2023-01-04 16:17:48 +0200

    Task 3 done

--- task_3.py
+++ task_3.py
@@ -3,3 +3,32 @@
 # Успадкувати від цього класу підклас Square і перевизначити метод get_area,
 # щоб він вираховував площу квадрата.
 
+print('--- Task 3 ---')
+
+
+class Parallelogram:
+    def __init__(self, width, length):
+        self.width = width
+        self.length = length
+
+    def get_area(self):
+        print('Calculate the area of the parallelogram:')
+        print(f' width = {self.width} cm\n length = {self.length} cm')
+        print(f'{self.width} * {self.length} = {self.width * self.length}')
+        return f'Area of the parallelogram = {self.width * self.length} cm'
+
+
+class Square(Parallelogram):
+    def get_area(self):
+        print('\nCalculate the area of the square:')
+        print(f' width = {self.width} cm\n length = {self.length} cm')
+        print(f'{self.width} * {self.length} = {self.width * self.length}')
+        return f'Area of the square = {self.width * self.length} cm'
+
+
+# Викликаю метод класу Parallelogram.
+area_1 = Parallelogram(5, 6)
+print(area_1.get_area())
+# Викликаю метод класу підкласу Square.
+area_2 = Square(4, 4)
+print(area_2.get_area())
